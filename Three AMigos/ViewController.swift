27ee5e48cc0f7008ec1f443186cbb4241355aa7d
Wood HE <wood@Woods-MacBook-Pro.local>
2017-09-19 10:53:56 -0400

    Initial Commit

---
+++ Three AMigos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Three AMigos
+//
+//  Created by Wood HE on 2017-09-19.
+//  Copyright © 2017 HE000060. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
